b49251f398a202af8dac29aab8474b8f30278932
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-11-29 21:59:27 +0100

    Fix error in notFound.ts

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 const notFoundHandler = (req: Request, res: Response): void => {
-    const {method, url} = req.body;
+    const {method, url} = req;
     res.status(404).json({
         message: `${method} ${url} not found`
     })
