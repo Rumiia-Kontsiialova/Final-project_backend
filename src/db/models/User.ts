f423a6c171232755951f7718cd1d006c608f14ac
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-12-01 21:36:09 +0100

    fixed  database object interface and schema hooks

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,20 +1,32 @@
 import { Schema, model, Document } from "mongoose";
-
 import { emailRegexp } from "../../constants/auth.constants.js";
-
 import { handleSaveError, setUpdateSettings } from "../hooks.js";
 
 export interface UserDocument extends Document {
     email: string;
+    fullname: string;
+    username: string;
     password: string;
     accessToken: string;
-    refreshToken: string
+    refreshToken: string;
+    createdAt: Date;
+    updateAt: Date;
 }
 
 const userSchema  = new Schema<UserDocument>({
     email: {
         type: String,
         match: emailRegexp,
+        unique: true,
+        required: true,
+    },
+    fullname: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    username: {
+        type: String,
         unique: true,
         required: true,
     },
@@ -29,13 +41,13 @@ const userSchema  = new Schema<UserDocument>({
     refreshToken: {
         type: String,
         required: true,
-    },
-});
+    }
+},
+    { versionKey: false, timestamps: true},
+);
 
 userSchema.post("save", handleSaveError);
-
 userSchema.pre("findOneAndUpdate", setUpdateSettings);
-
 userSchema.post("findOneAndUpdate", handleSaveError)
 
 const User = model<UserDocument>("user", userSchema);
