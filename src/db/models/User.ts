f8041522a1489794c015faf77a3b434ed8fc2f3c
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-12-01 18:36:26 +0100

    Added model, database object interface, and schema hooks

---
+++ src/db/models/User.ts
@@ -0,0 +1,43 @@
+import { Schema, model, Document } from "mongoose";
+
+import { emailRegexp } from "../../constants/auth.constants.js";
+
+import { handleSaveError, setUpdateSettings } from "../hooks.js";
+
+export interface UserDocument extends Document {
+    email: string;
+    password: string;
+    accessToken: string;
+    refreshToken: string
+}
+
+const userSchema  = new Schema<UserDocument>({
+    email: {
+        type: String,
+        match: emailRegexp,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    accessToken: {
+        type: String,
+        required: true,
+    },
+    refreshToken: {
+        type: String,
+        required: true,
+    },
+});
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", setUpdateSettings);
+
+userSchema.post("findOneAndUpdate", handleSaveError)
+
+const User = model<UserDocument>("user", userSchema);
+
+export default User;
